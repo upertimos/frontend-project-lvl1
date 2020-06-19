ae105f75f730c580836faf7016729f1e68584b42
upertimos <upertimos@gmail.com>
2020-06-19 09:21:44 +0300

    step 4th

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 export const greeting = console.log('Welcome to the Brain Games!');
-export const  userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+export const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName} !`);
